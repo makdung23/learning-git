3a4a389d0e9769bdb09761b1663e4845778facce
Your Name <makdung23@naver.com>
2019-08-26 04:57:22 +0000

    [HOTFIX]로그를 영어로 수정

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const port = 3000;
 
 http.createServer((request, response)=> {
 	
-	console.log('요청 수신.');
+	console.log('Request received.');
 	response.writeHead(200, {'Content-Type': 'text/html'});
 	response.write('Hello Git!');
 	response.end();
 }).listen(port);
 
-console.log(`서버 시작 - 포트:${port}`);
+console.log(`Server has stared - port:${port}`);
