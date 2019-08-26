89077b746b252014a3b11feb133b7f054c7d6ca7
Your Name <makdung23@naver.com>
2019-08-26 04:31:01 +0000

    Node HTTP 서버 작성

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-function main(){
-	console.log("Hello goorm!");
-}
+const http = require('http');
+const port = 3000;
 
-main();
+http.createServer(function(request, response) {
+	
+	console.log('요청 수신.');
+	response.writeHead(200, {'Content-Type': 'text/html'});
+	response.write('Hello Git!');
+	response.end();
+}).listen(port);
+
+console.log(`서버 시작 - 포트:${port}`);
